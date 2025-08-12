7b825afac9a1394713a31c4ae4863e2897d7cedb
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-08-12 19:17:59 +0100

    ApplicationRoleConnectionMetadataType

--- src/register_metadata.py
+++ src/register_metadata.py
@@ -1,5 +1,17 @@
 import requests
 import config
+from enum import Enum
+
+
+class ApplicationRoleConnectionMetadataType(Enum):
+    INTEGER_LESS_THAN_OR_EQUAL = 1  # 	the metadata value (integer) is less than or equal to the guild's configured value (integer)
+    INTEGER_GREATER_THAN_OR_EQUAL = 2  # 	the metadata value (integer) is greater than or equal to the guild's configured value (integer)
+    INTEGER_EQUAL = 3  # the metadata value (integer) is equal to the guild's configured value (integer)
+    INTEGER_NOT_EQUAL = 4  # the metadata value (integer) is not equal to the guild's configured value (integer)
+    DATETIME_LESS_THAN_OR_EQUAL = 5  # the metadata value (ISO8601 string) is less than or equal to the guild's configured value (integer; days before current date)
+    DATETIME_GREATER_THAN_OR_EQUAL = 6  # the metadata value (ISO8601 string) is greater than or equal to the guild's configured value (integer; days before current date)
+    BOOLEAN_EQUAL = 7  # the metadata value (integer) is equal to the guild's configured value (integer; 1)
+    BOOLEAN_NOT_EQUAL = 8
 
 
 def register_metadata():
@@ -10,7 +22,7 @@ def register_metadata():
             "key": "is_student",
             "name": "Current Student",
             "description": "Current student",
-            "type": 7,  # boolean_eq
+            "type": ApplicationRoleConnectionMetadataType.BOOLEAN_EQUAL,
         },
     ]
 
